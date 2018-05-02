def747bce9a1a04f8c77b1306ac3502909d116ad
Serizao <wleberre@eichromlab.com>
2018-05-02 16:31:32 +0200

    II

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 !#/bin/sh
 pkg update
-pkg -y install xorg
-pkg -y install xfce
+pkg install -y xorg
+pkg install -y xfce
 pkg install slim slim-themes
 echo 'dbus_enable="YES"' >> /etc/rc.conf
 echo 'hald_enable="YES"' >> /etc/rc.conf
 echo 'slim_enable="YES"' >> /etc/rc.conf
 cd
 echo 'exec startxfce4'>>.xinitrc
-
+pkg install -y wget git vim 
